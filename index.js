ef804576507db23f4340d5b186979f2263dee325
KennethJanLopez <lkennethjan@gmail.com>
2026-08-06 18:25:27 +0800

    anada commit

--- index.js
+++ index.js
@@ -1,26 +1,68 @@
 require("dotenv").config();
 
 const express = require("express");
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("GitHub Discord Bot online");
+const discord = new Client({
+  intents: [GatewayIntentBits.Guilds],
 });
 
-app.post("/github/webhook", (req, res) => {
-    const event = req.headers["x-github-event"];
+discord.once("ready", () => {
+  console.log(`Discord bot online as ${discord.user.tag}`);
+});
 
-    console.log("GitHub Event:", event);
-    console.log(req.body);
+discord.login(process.env.DISCORD_TOKEN);
 
-    res.status(200).send("received");
+// GitHub webhook
+app.post("/github/webhook", async (req, res) => {
+  const event = req.headers["x-github-event"];
+
+  console.log("GitHub Event:", event);
+
+  if (event === "issues") {
+    const action = req.body.action;
+    const issue = req.body.issue;
+
+    console.log({
+      action,
+      title: issue.title,
+      body: issue.body,
+    });
+
+    if (action === "opened") {
+      const channel = await discord.channels.fetch(
+        process.env.DISCORD_ISSUE_CHANNEL_ID,
+      );
+
+      await channel.threads.create({
+        name: `#${issue.number} ${issue.title}`,
+        message: {
+          content: `🟡 **New GitHub Issue**
+
+${issue.body || "No description"}
+
+GitHub:
+${issue.html_url}`,
+        },
+      });
+
+      console.log("Discord thread created");
+    }
+  }
+
+  res.sendStatus(200);
+});
+
+app.get("/", (req, res) => {
+  res.send("iGit bot online");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
